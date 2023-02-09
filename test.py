f4ad934b6f9628b3dfbd3a88f32d2de5bc4a759d
justinscandale <100536879+justinscandale@users.noreply.github.com>
2023-02-09 14:53:02 -0500

    commit 2

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='index')
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('index.html')
+if __name__ == '__main__':
+    app.run()
