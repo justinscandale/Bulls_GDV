f799e1025f7763a3bc9215602e3a1698e83a048b
justinscandale <100536879+justinscandale@users.noreply.github.com>
2023-02-18 16:23:15 -0500

    Make Python To act On CSV File

--- Main.py
+++ Main.py
@@ -2,22 +2,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET","POST"])
+@app.route('/home', methods=["GET","POST"])
 def rootPage():
-    height = ''
+    class_prefix = ''
+    class_number = ''
     weight = ''
     bmi = ''
     if request.method == "POST" and 'weight' in request.form:
-        height = float(request.form.get('height'))
-        weight = float(request.form.get('weight'))
-        bmi = calcBmi(height, weight)
+        class_prefix = float(request.form.get('height'))
+        class_number = float(request.form.get('weight'))
+        grade_dist = calcGradeDist(class_prefix, class_number)
     return render_template("index.html", height=height, weight=weight, bmi=bmi)
 
-def calcBmi(height, weight):
+def calcGradeDist(class_prefix, class_number):
+    professors = 
     return round((703* weight) / (height)**2,2)
 
 #flask by deault accepts get methods, must declare if u want more
-@app.route('/method', methods=['GET','POST'])
+@app.route('/results', methods=['GET','POST'])
 def method():
     if request.method == 'POST':
         return 'You have used a post request!'
